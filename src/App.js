5c4f3e10b52014104e9fd835487a09b058fc67e5
shivansh-aggarwal <shivansh.aggarwal@unthinkable.co>
2022-01-18 11:42:34 +0530

    adding form validation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
 import SignIn from './pages/SignIn';
+import SignInState from './context/SignInState';
+import {Route, useHistory} from 'react-router-dom';
+import {Fragment} from 'react';
+import UserDetails from './pages/UserDetails';
 
 function App() {
+  const history = useHistory();
+
   return (
-    <SignIn />
+    <Fragment>
+      <Route path="/" exact>
+        <SignInState>
+          <SignIn history={history}/>
+        </SignInState>
+      </Route>
+      <Route path="/user-details">
+        <UserDetails />
+      </Route>
+    </Fragment>
   )
 }
 
--- src/components/InputData.js
+++ src/components/InputData.js
@@ -3,10 +3,15 @@ import "./InputData.css";
 const InputData = (props) => {
     const tagName = props.tagName;
     const typeName = props.typeName;
+    const updateInputHandler = (event) => {
+        props.inputDataChange(event);
+    }
+
     return (
         <div>
             <label className={tagName}>{tagName}</label>
-            <input className="inputBox" type={typeName}/>
+            <input className="inputBox" type={typeName} 
+                    name={tagName} onChange={updateInputHandler}/>
         </div>
     )
 }
